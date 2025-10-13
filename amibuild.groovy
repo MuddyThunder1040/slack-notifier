07cb081b13dc2603f5a72799b6091c29d7878383
MuddyThunder1040 <vishnupardha1040@gmail.com>
2025-10-12 23:51:05 -0700

    Add initial Jenkins pipeline configuration for AMI build process

---
+++ amibuild.groovy
@@ -0,0 +1,38 @@
+pipeline {
+  agent any
+
+  environment {
+    AWS_REGION = 'us-east-1'
+  }
+
+  parameters {
+    string(name: 'BRANCH', defaultValue: 'main', description: 'Branch of aws-topology repo')
+  }
+
+  stages {
+    stage('Setup') {
+      steps {
+        cleanWs()
+        git branch: "${params.BRANCH}", url: 'https://github.com/MuddyThunder1040/aws-topology.git'
+      }
+    }
+
+    stage('Build AMI') {
+      steps {
+        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'Aws-cli']]) {
+          sh '''
+            terraform init
+            terraform apply -target=module.ami_builder -auto-approve
+            terraform output ami_id
+          '''
+        }
+      }
+    }
+  }
+
+  post {
+    always {
+      cleanWs()
+    }
+  }
+}
